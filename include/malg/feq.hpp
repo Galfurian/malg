6d43c202ba29f81b207abdedf37478101376a0f2
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2023-12-07 16:30:50 -0500

    Add more floating point comparison functions

--- include/malg/feq.hpp
+++ include/malg/feq.hpp
@@ -12,7 +12,7 @@ namespace malg::feq
 
 static inline auto &tolerance()
 {
-    static double tol = std::numeric_limits<double>::epsilon();
+    static double tol = 1e-06;
     return tol;
 }
 
@@ -27,4 +27,37 @@ inline bool approximately_equal(T1 a, T2 b)
     return std::fabs(a - b) <= tolerance() * std::fmax(std::fabs(a), std::fabs(b));
 }
 
-} // namespace feq
+/// @brief Checks if the fir floating point value is lesser than or equal to the second one.
+/// @param a the first value.
+/// @param b the second value.
+/// @returns true if (a <= b).
+/// @returns false otherwise.
+template <typename T1, typename T2>
+inline bool approximately_lesser_than_equal(T1 a, T2 b)
+{
+    return (a < b) || (malg::feq::approximately_equal(a, b));
+}
+
+/// @brief Checks if the fir floating point value is greater than or equal to the second one.
+/// @param a the first value.
+/// @param b the second value.
+/// @returns true if (a >= b).
+/// @returns false otherwise.
+template <typename T1, typename T2>
+inline bool approximately_greater_than_equal(T1 a, T2 b)
+{
+    return (a > b) || (malg::feq::approximately_equal(a, b));
+}
+
+/// @brief Checks if the two floating point values are equal.
+/// @param a the first value.
+/// @param b the second value.
+/// @returns true if they are approximately equal.
+/// @returns false otherwise.
+template <typename T>
+inline bool approximately_equal_to_zero(T a)
+{
+    return std::fabs(a) <= malg::feq::tolerance() * std::fabs(a);
+}
+
+} // namespace malg::feq
